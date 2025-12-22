fix magnitude_plotter_with_flip_angle.py to correct time calculation in time_all extension

magnitude_plotter_with_flip_angle.py
@@ -79,7 +79,7 @@ def calc_magnetization(config, t1, angle):
         time = np.linspace(0, config["tr"], config["sample_num"])
         mz = t1_r(time)
 
-        time_all.extend(list(time + (t+1) * config["tr"]))
+        time_all.extend(list(time + t * config["tr"]))
         longitudinal.extend(mz)
         m0_pre = mz[-1]
     return time_all, longitudinal