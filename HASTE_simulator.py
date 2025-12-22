5293c8b4192b0b05d0f6c06258f347d8b90929fd
ChuWeiChang <chuweichang0319@gmail.com>
2025-12-22 17:22:16 +0800

    refactor HASTE_simulator.py to improve k-space modification and enhance image comparison for multiple T2 values

--- HASTE_simulator.py
+++ HASTE_simulator.py
@@ -9,31 +9,28 @@ def main(config):
 
     k = np.fft.fftshift(np.fft.fft2(img))
 
-    fig, axs = plt.subplots(1, len(config["t2"] + 1), figsize=(10, 4))
+    fig, axs = plt.subplots(1, len(config["t2"]) + 1, figsize=(10, 4))
 
     axs[0].imshow(img, cmap="gray")
     axs[0].set_title("Original")
     axs[0].axis("off")
+    for i, t2 in enumerate(config["t2"]):
+        k_modified = np.zeros_like(k)
+        t2_filter = lambda m, t: m * np.exp(-t / t2)
 
+        time = config["te"]
+        esp = config["esp"]
+        for phase_encode in range(round(k.shape[0] / 2)):
+            for freq_encode in range(k.shape[1]):
+                k_modified[phase_encode][freq_encode] = t2_filter(k[phase_encode][freq_encode], time)
+                time += esp
 
-    k_modified = np.zeros_like(k)
-    t2_filter = lambda m, t: m * np.exp(-t / config["t2"])
+        img_mod_cplx = np.fft.ifft2(np.fft.ifftshift(k_modified))
+        img_mod_mag = np.abs(img_mod_cplx)
 
-    time = config["te"]
-    esp = config["esp"]
-    for phase_encode in range(k.shape[0] / 2):
-        for freq_encode in range(k.shape[1]):
-            k_modified[phase_encode][freq_encode] = t2_filter(k[phase_encode][freq_encode], time)
-            time += esp
-
-    img_mod_cplx = np.fft.ifft2(np.fft.ifftshift(k_modified))
-    img_mod_mag = np.abs(img_mod_cplx)
-
-
-
-    axs[1].imshow(img_mod_mag, cmap="gray")
-    axs[1].set_title("Modified Magnitude")
-    axs[1].axis("off")
+        axs[i+1].imshow(img_mod_mag, cmap="gray")
+        axs[i+1].set_title(f"t2 = {t2}ms")
+        axs[i+1].axis("off")
 
     plt.tight_layout()
     plt.show()
@@ -55,13 +52,18 @@ if __name__ == "__main__":
         type=int,
         default=4,
     )
+    parser.add_argument(
+        "--te",
+        type=int,
+        default=20,
+    )
 
     args = parser.parse_args()
-    config = {
+    configs = {
         "size": args.size,
         "te": args.te,
         "t2": [60,120,180,240],
         "esp": 4,
     }
 
-    main(config)
+    main(configs)
